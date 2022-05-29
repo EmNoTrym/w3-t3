e6eff637606aa9521f048dcd734b2a9281446b3a
EmNoTrym <63888869+EmNoTrym@users.noreply.github.com>
2022-05-29 12:09:12 +0700

    swap-item

--- Assets/Scripts/EquipSlot.cs
+++ Assets/Scripts/EquipSlot.cs
@@ -7,6 +7,13 @@ public class EquipSlot : MonoBehaviour, IDropHandler
 {
     public void OnDrop(PointerEventData eventData)
     {
+        if (gameObject.transform.Find("ItemButton"))
+        {
+            Transform itemButton = gameObject.transform.Find("ItemButton");
+            itemButton.GetComponent<RectTransform>().anchoredPosition = eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition;
+            itemButton.SetParent(eventData.pointerDrag.transform.parent);
+            itemButton.localPosition = gameObject.transform.Find("EmptyButton").GetComponent<RectTransform>().localPosition;
+        }
         eventData.pointerDrag.transform.SetParent(transform);
         eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = GetComponent<RectTransform>().anchoredPosition;
         eventData.pointerDrag.GetComponent<RectTransform>().localPosition = Vector3.zero;
